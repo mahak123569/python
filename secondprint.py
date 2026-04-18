ff22a4c4d6cbd21c90ae02c021d33fe5681d8473
mahak <kaushikikes@gmail.com>
2026-04-18 23:10:28 +0530

    update

--- secondprint.py
+++ secondprint.py
@@ -2,7 +2,7 @@ str1 ="collag"
 str2 ='this is a string'
 str3 ="""this is a word"""
 
-final_str =str1+str2
+final_str =str1+"   "+str2
 print(final_str)
 
 str4 ="this is a can .\nnew car gifted by me."
