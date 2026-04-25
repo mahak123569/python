4417af29b72169e97f9a604bc8b56b7f77d723bf
mahak <kaushikikes@gmail.com>
2026-04-25 21:31:09 +0530

    add new

--- questionsum.py
+++ questionsum.py
@@ -1,10 +1,14 @@
-marks = [94.4, 87.5, 95.2, 66.4, 45.1]
-print(marks)
-print(len(marks))
-print(marks[0])
-print(marks[1])
+# marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+# print(marks)
+# print(len(marks))
+# print(marks[0])
+# print(marks[1])
 
-student = ["Karan" , 95.4,17,"Delhi"]
-print(student[0])
-student[0] = "arjun"
-print(student)
+# student = ["Karan" , 95.4,17,"Delhi"]
+# print(student[0])
+# student[0] = "arjun"
+# print(student)
+
+#list slicing
+marks = [85, 94, 76, 63,48]
+print(marks[-3:-1])
